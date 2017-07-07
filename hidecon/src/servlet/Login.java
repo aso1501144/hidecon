5c504be49dc01f27de9329767ce9b91ad0ae14db
hide <1501153@st.asojuku.ac.jp>
2017-07-07 10:44:09 +0900

    ( ˘•ω•˘ )

--- hidecon/src/servlet/Login.java
+++ hidecon/src/servlet/Login.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.EntryDAO;
+import dao.UserDAO;
+import model.EntryBean;
 
 /**
  * Servlet implementation class login
@@ -28,9 +34,11 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/G101login.jsp");
+
+		HttpSession session = request.getSession(true);
+		session.invalidate();
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/G101login.jsp");
 		rd.forward(request, response);
 
 	}
@@ -39,8 +47,61 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
+		String path; // 分岐先のファイル名
+		HttpSession session = request.getSession();
+		// ユーザーID、パスワードの取り出し
+
+		String get = request.getParameter("s_id");
+		int s_id = 0;
+		if (number(get)) {
+			s_id = Integer.parseInt(get);
+		} else {
+			request.setAttribute("errorMassage", "学生IDまたはパスワードが違います。");
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/userLogin.jsp");
+			rd.forward(request, response);
+			return;
+		}
+
+		String pass = request.getParameter("pass");
+		UserDAO userDAO = new UserDAO();
+		//UserBean user = new UserBean();
+
+		String login = userDAO.getLogin(s_id, pass);
+
+		if (login != null) {
+			// 会員情報をセッションに格納
+			session.setAttribute("s_id", login);
+			session.setAttribute("s_name", userDAO.getSname(s_id));
+
+			EntryDAO entry = new EntryDAO();
+			ArrayList<EntryBean> entryList = new ArrayList<EntryBean>();
+			entryList = entry.getData(s_id);
+
+			session.setAttribute("entry", entryList);
+
+			path = "WEB-INF/jsp/G102gazou.jsp";
+
+		} else {
+			System.out.println("ログイン失敗");
+			request.setAttribute("errorMassage", "学生IDまたはパスワードが違います。");
+			path = "WEB-INF/jsp/G101login.jsp";
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
+
+	}
+
+	public boolean number(String num) {
+		try {
+			Integer.parseInt(num);
+			return true;
+		} catch (NumberFormatException e) {
+			// TODO: handle exception
+			return false;
+		}
+
+	}
 	}
 
 }
